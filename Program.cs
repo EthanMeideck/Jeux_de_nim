94cdc3a630958e4e3993168f7cdc04313d0495e7
NoRushJustPanda <ethan@meideck.net>
2025-05-13 12:04:23 +0200

    Optimisation et organisation méthode setup

--- Program.cs
+++ Program.cs
@@ -6,16 +6,24 @@ namespace Jeux
     public class Jeux_nim
     {
         // Attributs du jeu
-        public int nombre_batonnets = 20;
-        public bool tour = true;
+        public int nombre_batonnets;
+        public bool tour; // Aussi appelé drapeau ou flag. Permettra de continuer ou non le jeu
+
+        public Jeux_nim()
+        {
+            // Initialisation des paramètres du jeu
+            Setup();
+        }
 
         public void Setup()
-            {
-            string affichage_batonnets = string.Concat(Enumerable.Repeat("# ", nombre_batonnets));
+        {
+            nombre_batonnets = 20;
+            tour = true;
+            // Affichage de tous les batonnets
+            string affichage_batonnets = string.Concat(Enumerable.Repeat("# ", nombre_batonnets)); // Permet d'afficher x éléments
             Console.Write(affichage_batonnets);
             Console.Write("\n" + affichage_batonnets);
             Console.Write("\n" + affichage_batonnets);
-
         }
     }
 
@@ -24,7 +32,6 @@ namespace Jeux
         static void Main(string[] args)
         {
             Jeux_nim t = new Jeux_nim();
-            t.Setup();
         }
 
     }
