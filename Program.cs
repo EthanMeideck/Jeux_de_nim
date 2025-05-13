fd4122028b8937600f24edd34c9d53c6a3cacf9d
NoRushJustPanda <ethan@meideck.net>
2025-05-13 11:32:31 +0200

    Fonction setup avec affichage des batonnets

--- Program.cs
+++ Program.cs
@@ -1,23 +1,30 @@
 ﻿using System;
+using System.Linq;
 
 namespace Jeux
 {
     public class Jeux_nim
     {
+        // Attributs du jeu
         public int nombre_batonnets = 20;
         public bool tour = true;
 
-        public Jeux_nim()
+        public void Setup()
             {
+            string affichage_batonnets = string.Concat(Enumerable.Repeat("# ", nombre_batonnets));
+            Console.Write(affichage_batonnets);
+            Console.Write("\n" + affichage_batonnets);
+            Console.Write("\n" + affichage_batonnets);
 
-            }
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-
+            Jeux_nim t = new Jeux_nim();
+            t.Setup();
         }
 
     }
